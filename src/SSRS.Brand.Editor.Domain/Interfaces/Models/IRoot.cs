c08c4a3e3150129cb08aa45c8129f30ed8c7b8ce
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-08-31 21:11:37 +0200

    feature: add branding models
    changes:
    - project fixes
    - models added
    - interfaces added
    
    closes #1

---
+++ src/SSRS.Brand.Editor.Domain/Interfaces/Models/IRoot.cs
@@ -0,0 +1,9 @@
+﻿namespace SSRS.Brand.Editor.Domain.Models;
+
+public interface IRoot
+{
+	Interface Interface { get; set; }
+	string Name { get; set; }
+	Theme Theme { get; set; }
+	string Version { get; set; }
+}
