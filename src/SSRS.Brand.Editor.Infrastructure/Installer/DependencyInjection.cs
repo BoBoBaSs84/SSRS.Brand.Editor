443e497757b461ed54d0baadfaf3243198395843
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-09-28 17:51:06 +0200

    feat: infrastructure file service
    changes:
    - file service added
    - file service interface added
    - file service exception added
    
    closes #12

--- src/SSRS.Brand.Editor.Infrastructure/Installer/DependencyInjection.cs
+++ src/SSRS.Brand.Editor.Infrastructure/Installer/DependencyInjection.cs
@@ -17,6 +17,7 @@ public static class DependencyInjection
 	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
 	{
 		services.AddLoggerService();
+		services.AddSingletonServices();
 
 		return services;
 	}
---
+++ src/SSRS.Brand.Editor.Infrastructure/Services/FileService.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Logging;
+
+using SSRS.Brand.Editor.Application.Interfaces.Infrastructure.Services;
+using SSRS.Brand.Editor.Domain.Exceptions;
+
+namespace SSRS.Brand.Editor.Infrastructure.Services;
+
+/// <summary>
+/// The file service class.
+/// </summary>
+internal sealed class FileService : IFileService
+{
+	private readonly ILoggerService<FileService> _loggerService;
+
+	private static readonly Action<ILogger, Exception?> LogException =
+		LoggerMessage.Define(LogLevel.Error, 0, "Exception occured.");
+
+	/// <summary>
+	/// Initilizes an instance of the file service class.
+	/// </summary>
+	/// <param name="loggerService">The logger service to use.</param>
+	public FileService(ILoggerService<FileService> loggerService)
+		=> _loggerService = loggerService;
+
+	public byte[] Load(string filePath)
+	{
+		try
+		{
+			byte[] content = File.ReadAllBytes(filePath);
+
+			return content;
+		}
+		catch (Exception ex)
+		{
+			_loggerService.Log(LogException, ex);
+			throw new FileServiceException($"Could not load file: '{filePath}'", ex);
+		}
+	}
+
+	public byte[] Load(string path, string fileName)
+	{
+		string filePath = Path.Combine(path, fileName);
+
+		return Load(filePath);
+	}
+
+	public bool Save(string filePath, byte[] content)
+	{
+		try
+		{
+			File.WriteAllBytes(filePath, content);
+
+			return File.Exists(filePath);
+		}
+		catch (Exception ex)
+		{
+			_loggerService.Log(LogException, ex);
+			throw new FileServiceException($"Could not save file: '{filePath}'", ex);
+		}
+	}
+
+	public bool Save(string path, string fileName, byte[] content)
+	{
+		string filePath = Path.Combine(path, fileName);
+
+		return Save(filePath, content);
+	}
+}
