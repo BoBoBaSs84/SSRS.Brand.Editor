14a492fc88958af9de1c5d150aa4499efbf240c9
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-09-28 18:38:15 +0200

    well started

---
+++ tests/SSRS.Brand.Editor.InfrastructureTests/InfrastructureTestBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using SSRS.Brand.Editor.Infrastructure.Installer;
+
+namespace SSRS.Brand.Editor.InfrastructureTests;
+
+public class InfrastructureTestBase
+{
+	private readonly IServiceProvider _serviceProvider;
+
+	public InfrastructureTestBase()
+	{
+		IHost host = CreateHostBuilder().Build();
+		_serviceProvider = host.Services;
+	}
+
+	public T GetService<T>() where T : class =>
+		_serviceProvider.GetRequiredService(typeof(T)) is not T service
+		? throw new ArgumentException($"{typeof(T)} needs to be registered.")
+		: service;
+
+	private static IHostBuilder CreateHostBuilder()
+		=> Host.CreateDefaultBuilder()
+		.ConfigureServices((context, services) =>
+		{
+			_ = services.AddInfrastructureServices();
+		});
+}
---
+++ tests/SSRS.Brand.Editor.InfrastructureTests/Services/FileServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using SSRS.Brand.Editor.Application.Interfaces.Infrastructure.Services;
+using SSRS.Brand.Editor.Domain.Exceptions;
+
+namespace SSRS.Brand.Editor.InfrastructureTests.Services;
+
+[TestClass]
+public class FileServiceTests : InfrastructureTestBase
+{
+	private readonly IFileService _fileService;
+
+	public FileServiceTests()
+		=> _fileService = GetService<IFileService>();
+
+	[TestMethod]
+	public void LoadTest()
+	{
+		Assert.Fail();
+	}
+
+	[TestMethod]
+	public void LoadExceptionTest()
+	{
+		Assert.ThrowsException<FileServiceException>(() => _fileService.Load(string.Empty));
+	}
+
+	[TestMethod]
+	public void SaveTest()
+	{
+		Assert.Fail();
+	}
+
+	[TestMethod]
+	public void SaveExceptionTest()
+	{
+		Assert.ThrowsException<FileServiceException>(() => _fileService.Save(string.Empty, Array.Empty<byte>()));
+	}
+}
