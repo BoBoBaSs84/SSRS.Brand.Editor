37c41defce69874382f7b6af2f967964eedeaa9d
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-01-07 13:30:19 +0100

    fix: small status fixes

--- src/SSRS.Brand.Editor.Application/ViewModels/MainViewModel.cs
+++ src/SSRS.Brand.Editor.Application/ViewModels/MainViewModel.cs
@@ -10,22 +10,19 @@ namespace SSRS.Brand.Editor.Application.ViewModels;
 /// </summary>
 public sealed class MainViewModel : NotifyPropertyBase
 {
-	private IRelayCommand _aboutCommand;
-	private IRelayCommand _exitCommand;
-	private IRelayCommand _newCommand;
-	private IRelayCommand _saveCommand;
+	private IRelayCommand? _aboutCommand;
+	private IRelayCommand? _exitCommand;
+	private IRelayCommand? _openCommand;
+	private IRelayCommand? _newCommand;
+	private IRelayCommand? _saveCommand;
+	private string _status = string.Empty;
 
-	/// <summary>
-	/// Initilizes an instance of the <see cref="MainViewModel"/> class.
-	/// </summary>
-	public MainViewModel()
-	{ }
 
 	/// <summary>
 	/// The command to show the about window.
 	/// </summary>
 	public IRelayCommand AboutCommand
-		=> _aboutCommand ??= new RelayCommand(() => { });
+		=> _aboutCommand ??= new RelayCommand(() => { StatusText = "Abouting .."; });
 
 	/// <summary>
 	/// The command to exit the application.
@@ -34,14 +31,25 @@ public sealed class MainViewModel : NotifyPropertyBase
 		=> _exitCommand ??= new RelayCommand(() => Environment.Exit(0));
 
 	/// <summary>
+	/// The command to open an existing brand.
+	/// </summary>
+	public IRelayCommand OpenCommand
+		=> _openCommand ??= new RelayCommand(() => { StatusText = "Opening .."; });
+
+	/// <summary>
 	/// The command to create a new brand.
 	/// </summary>
 	public IRelayCommand NewCommand
-		=> _newCommand ??= new RelayCommand(() => { });
+		=> _newCommand ??= new RelayCommand(() => { StatusText = "Creating .."; });
 
 	/// <summary>
 	/// The command to save the current brand.
 	/// </summary>
 	public IRelayCommand SaveCommand
-		=> _saveCommand ??= new RelayCommand(() => { });
+		=> _saveCommand ??= new RelayCommand(() => { StatusText = "Saving .."; });
+
+	/// <summary>
+	/// The current status text of the view.
+	/// </summary>
+	public string StatusText { get => _status; private set => SetProperty(ref _status, value); }
 }
