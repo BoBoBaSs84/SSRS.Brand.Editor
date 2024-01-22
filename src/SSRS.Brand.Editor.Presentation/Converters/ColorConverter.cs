c7877ec6cd3b35eba7d3be358a90522dd0c0dfcb
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-01-22 16:39:28 +0100

    feat: converter for color property binding
    changes:
    - added `ColorConverter` class
    - modified user control
    
    closes #48

---
+++ src/SSRS.Brand.Editor.Presentation/Converters/ColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+using DColor = System.Drawing.Color;
+using MColor = System.Windows.Media.Color;
+
+namespace SSRS.Brand.Editor.Presentation.Converters;
+
+/// <summary>
+/// The color converter class.
+/// </summary>
+[ValueConversion(typeof(DColor), typeof(MColor))]
+internal sealed class ColorConverter : IValueConverter
+{
+	/// <inheritdoc/>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		DColor color = (DColor)value;
+		return MColor.FromArgb(color.A, color.R, color.G, color.B);
+	}
+
+	/// <inheritdoc/>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		MColor color = (MColor)value;
+		return DColor.FromArgb(color.A, color.R, color.G, color.B);
+	}
+}
