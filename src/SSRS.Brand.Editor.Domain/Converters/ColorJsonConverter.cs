0a0eab90909329ce2a081098276340d4f7fd71c3
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-08-24 18:44:24 +0200

    feat: colot json converter fix

--- src/SSRS.Brand.Editor.Domain/Converters/ColorJsonConverter.cs
+++ src/SSRS.Brand.Editor.Domain/Converters/ColorJsonConverter.cs
@@ -2,6 +2,8 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
+using BB84.Extensions;
+
 namespace SSRS.Brand.Editor.Domain.Converters;
 
 /// <summary>
@@ -16,41 +18,10 @@ public sealed class ColorJsonConverter : JsonConverter<Color>
 	public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
 		string? value = reader.GetString();
-		return value is null ? Color.White : GetColor(value);
+		return value is null ? Color.White : value.FromRGBHexString();
 	}
 
 	/// <inheritdoc/>
 	public override void Write(Utf8JsonWriter writer, Color value, JsonSerializerOptions options)
-		=> writer.WriteStringValue($"#{value.R:X2}{value.G:X2}{value.B:X2}");
-
-	private static Color GetColor(string value)
-	{
-		Color color = Color.Empty;
-
-		if (value[0].Equals('#') && ((value.Length == 7) || (value.Length == 4)))
-		{
-			if (value.Length == 7)
-			{
-				color = Color.FromArgb(
-					Convert.ToInt32(value.Substring(1, 2), 16),
-					Convert.ToInt32(value.Substring(3, 2), 16),
-					Convert.ToInt32(value.Substring(5, 2), 16)
-					);
-			}
-			else
-			{
-				string r = char.ToString(value[1]);
-				string g = char.ToString(value[2]);
-				string b = char.ToString(value[3]);
-
-				color = Color.FromArgb(
-					Convert.ToInt32(r + r, 16),
-					Convert.ToInt32(g + g, 16),
-					Convert.ToInt32(b + b, 16)
-					);
-			}
-		}
-
-		return color;
-	}
+		=> writer.WriteStringValue(value.ToRGBHexString());
 }
